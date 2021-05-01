1ed9e45fc5faca697f2933f1c18adfd002fe47a6
Heliferepo <mattisdalleau@gmail.com>
2021-05-01 14:48:28 +0400

    remove GLIBC unauthorized

--- vm/main.c
+++ vm/main.c
@@ -17,10 +17,9 @@ int main(int ac UNUSED, char **av UNUSED)
     if (ac == 1)
         return (84);
     vm_init(&vm, av + 1);
-    //write(1, &vm.champion[0].header, sizeof(struct header_s));
-    printf("0x%x\n", vm.champion[0].header.magic);
-    printf("%s\n", vm.champion[0].header.prog_name);
-    printf("%d\n", vm.champion[0].header.prog_size);
-    printf("%s\n", vm.champion[0].header.comment);
+    eprintf("magic: [0x%x]\n", vm.champion[0].header.magic);
+    eprintf("prog_name: [%s]\n", vm.champion[0].header.prog_name);
+    eprintf("prog_size: [%d]\n", vm.champion[0].header.prog_size);
+    eprintf("comment: [%s]\n", vm.champion[0].header.comment);
     return (0);
 }
