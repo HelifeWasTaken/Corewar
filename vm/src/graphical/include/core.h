70123be1171f69c445329af02be3d5aafab8a31c
thibault <thibault.dufaure-de-citres@epitech.eu>
2021-05-16 11:56:00 +0400

    rm cpp file remaned c file and make for graph

---
+++ vm/src/graphical/include/core.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** corewar
+** File description:
+** core.h
+*/
+
+#ifndef CORE_H
+    #define CORE_H
+
+    typedef struct core {
+
+    } core_t;
+
+void init_core(void);
+
+#endif
---
+++ vm/src/graphical/src/main.c
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2021
+** main.c
+** File description:
+** corewar
+*/
+
+#include <SFML/Graphics.h>
+
+int main(void)
+{
+    return (0);
+}
