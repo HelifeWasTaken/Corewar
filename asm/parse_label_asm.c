683f014cd38f057d179daf8ba09bfbf695a60e97
Heliferepo <mattisdalleau@gmail.com>
2021-05-21 12:06:18 +0400

    fix check label asm

--- asm/parse_label_asm.c
+++ asm/parse_label_asm.c
@@ -12,8 +12,12 @@ bool parse_label_asm(parser_t *parser, char *buffer, instruction_t *ins,
 {
     size_t i = parser->col;
 
-    skip_spaces(parser, buffer);
-    for (; buffer[i] && !istoken(buffer[i], ", #"); i++);
+    for (; buffer[i] && !istoken(buffer[i], ", #"); i++) {
+        if (!istoken(buffer[i], LABEL_CHARS)) {
+            efprintf(stderr, "Invalid Label token at %d:%d\n", parser->line, i);
+            return (false);
+        }
+    }
     ins->param[ins->arg_count].is_jump_not_complete = true;
     ins->param[ins->arg_count].is = estrndup(&buffer[parser->col],
             i - parser->col);
