50afdfee21ff846a763ba3c3e4850729fe434695
Raphael Salaverria <TinosS@localhost.localdomain>
2021-05-23 16:26:50 +0400

    add sch with fork but not alive

---
+++ corewar/src/champion/champions/sch.s
@@ -0,0 +1,77 @@
+.name "sch"
+.comment "mille deux c'est insultant"
+
+m9:
+ld %667, r1
+sti r2, %:m9, %9
+fork %:m4
+ld %91216, r6
+ld %22556762, r7
+ld %357338, r12
+ld %0, r2
+
+p99:
+live %9
+fork %:ak
+
+maria:
+live %9
+st r12, 456
+st  r13, 478
+st r14, 489
+st r15, 493
+st r9, 255
+st r9, 522
+st r9, 369
+st r9, 213
+st r9, 150
+st r9, 91
+zjmp %:maria
+
+ak:
+live %9
+fork %:mp4
+
+m4:
+live %9
+fork %:mp4
+
+cc:
+live %9
+fork %:mp4
+
+g36:
+live %9
+fork %:a7
+
+a7:
+ld %6554564, r4
+zjmp %:ak
+sti r1, %:live, %13
+and r1, %0, r1
+live: live %13
+zjmp %:live
+
+mp4:
+live %9
+fork %:jozef
+
+jozef:
+live %9
+fork %:rolex
+live %9
+ld %190054658, r1
+ld %6, r2
+ld %4, r3
+ld %0, r9
+zjmp %:audemard
+
+rolex:
+live %9
+st r2, 123
+st r3, 127
+st r9, 132
+zjmp %:audemard
+
+audemard:
+sti r1, r2, r10
