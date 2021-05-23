af0334671192450b0ffe71878f01fdc2e07d7dcb
Heliferepo <mattisdalleau@gmail.com>
2021-05-23 22:57:22 +0400

    restored vm

--- bonus/src/draw.c
+++ bonus/src/draw.c
@@ -21,7 +21,7 @@ static const char *WINDOWS_LOGO[] = {
     "||   _    |  _--\"\"--_|             |  |              ||",
     "||     \" --\"\"                       --               ||",
     "||                                                   ||",
-    "=======================================================",
+    "======================================================",
     NULL
 };
 
@@ -36,7 +36,7 @@ void draw_hud(vm_t *vm)
             " Champion count: [%d]\n",
         vm->last_live, vm->live_count, vm->cycle_to_die, vm->champion_count);
     for (unsigned int i = 0; i < vm->champion_count; i++)
-        printw("%s[ Player: [%d], Name: [%s] ]", i ? "   " : "",
+        printw(" [ Player: [%d], Name: [%s] ]",
             i, vm->champion[i].header.prog_name);
     for (unsigned int i = 0; WINDOWS_LOGO[i]; i++)
         mvprintw(y++, COLS / 2 - 27, "%s", WINDOWS_LOGO[i]);
@@ -52,14 +52,14 @@ static void draw_champion_internal(proc_t *proc, int y)
         return;
     for (; i != proc->pc.addr && lign + 2 < ROW_HUD; i++) {
         attron(COLOR_PAIR(get_color_champion(proc->player)));
-        col += 2;
-        if (2 + col >= COL_MAX) {
+        col++;
+        if (3 + (col * 3) >= COL_MAX) {
             col = DEF_COL;
             lign += 2;
         }
     }
     if (i == proc->pc.addr)
-        mvprintw(lign, col, "X");
+        mvprintw((2 + lign), 3 + (col * 3), "XX");
 }
 
 void draw_champion(virtual_machine_t *vm, int y)
@@ -76,12 +76,14 @@ void draw_map(virtual_machine_t *vm, int y)
     int i = COLS * y;
 
     attron(COLOR_PAIR(10));
-    for (; i < MEM_SIZE && lign + 1 < ROW_HUD; i++) {
+    mvprintw(2, 0, "0x%04x", i);
+    for (; i < MEM_SIZE && lign + 2 < ROW_HUD; i++) {
         attron(COLOR_PAIR(get_color_player(vm->memory[i].player)));
-        mvprintw(lign, col, " ");
-        col += 2;
-        if (2 + col >= COL_MAX) {
+        mvprintw((2 + lign), 3 + (col * 3), "%02x", vm->memory[i].byte);
+        col++;
+        if (3 + (col * 3) >= COL_MAX) {
             attron(COLOR_PAIR(10));
+            mvprintw((2 + lign), 0, "0x%02x", i);
             col = DEF_COL;
             lign += 2;
         }
